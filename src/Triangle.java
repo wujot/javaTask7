f9bcbb47eb38e3d4385ea2018d4762f6e3f8dcc3
wujot <wojciech.wujot"gmail.com>
2018-01-21 15:47:07 +0100

    Class Triangle was created to represent triangle according to JavaBeans style.

---
+++ src/Triangle.java
@@ -0,0 +1,39 @@
+public class Triangle {
+    private int sideA;
+    private int sideB;
+    private int sideC;
+
+    Triangle() {
+
+    }
+
+    Triangle(int sideA, int sideB, int sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public int getSideA() {
+        return sideA;
+    }
+
+    public int getSideB() {
+        return sideB;
+    }
+
+    public int getSideC() {
+        return sideC;
+    }
+
+    public void setSideA(int sideA) {
+        this.sideA = sideA;
+    }
+
+    public void setSideB(int sideB) {
+        this.sideB = sideB;
+    }
+
+    public void setSideC(int sideC) {
+        this.sideC = sideC;
+    }
+}
