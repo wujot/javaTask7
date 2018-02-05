2ad9bbb396ee52bb0b2b4cc6c434d8e96cd4835f
wujot <wojciech.wujot"gmail.com>
2018-02-05 23:20:16 +0100

    Code was corrected according to slack instructions.

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -3,11 +3,16 @@ public class RightTriangle {
     Triangle triangle = new Triangle();
 
     public boolean isRightTriangle(Triangle triangle) {
-        if((triangle.getSideA() * triangle.getSideA()) + (triangle.getSideB() * triangle.getSideB()) ==
+        if ((triangle.getSideA() * triangle.getSideA()) + (triangle.getSideB() * triangle.getSideB()) ==
                 (triangle.getSideC() * triangle.getSideC())) {
-            return  true;
-        }else {
+            return true;
+        } else if (((triangle.getSideA() * triangle.getSideA()) + (triangle.getSideC() * triangle.getSideC()) ==
+                (triangle.getSideB() * triangle.getSideB()))) {
+            return true;
+        } else if (((triangle.getSideB() * triangle.getSideB()) + (triangle.getSideC() * triangle.getSideC()) ==
+                (triangle.getSideA() * triangle.getSideA())))
+            return true;
+        else
             return false;
-        }
     }
 }
--- src/RightTriangleTest.java
+++ src/RightTriangleTest.java
@@ -5,24 +5,36 @@ public class RightTriangleTest {
         RightTriangle rightTriangle = new RightTriangle();
 
         Triangle triangle1 = new Triangle();
-        triangle1.setSideA(2);
-        triangle1.setSideB(2);
-        triangle1.setSideC(3);
+        triangle1.setSideA(13);
+        triangle1.setSideB(12);
+        triangle1.setSideC(5);
 
         Triangle triangle2 = new Triangle();
         triangle2.setSideA(3);
         triangle2.setSideB(4);
         triangle2.setSideC(5);
 
+        Triangle triangle3 = new Triangle();
+        triangle3.setSideA(5);
+        triangle3.setSideB(7);
+        triangle3.setSideC(12);
+
         // check frist traingle
-        if(rightTriangle.isRightTriangle(triangle1) == true) {
+            if(rightTriangle.isRightTriangle(triangle1)) {
             System.out.println("Is right trinagle.");
         }else {
             System.out.println("Is not right triangle.");
         }
 
         // check second triangle
-        if(rightTriangle.isRightTriangle(triangle2) == true) {
+        if(rightTriangle.isRightTriangle(triangle2)) {
+            System.out.println("Is right trinagle.");
+        }else {
+            System.out.println("Is not right triangle.");
+        }
+
+        // check thirth triangle
+        if(rightTriangle.isRightTriangle(triangle3)) {
             System.out.println("Is right trinagle.");
         }else {
             System.out.println("Is not right triangle.");
